361f3ccc9386c992efa9999b8ac0ded461e9dd5a
arif-z04 <smarifuzzaman123@gmail.com>
2024-09-09 16:24:46 +0600

    made some changes to this file

--- Pointers/19_program.c
+++ Pointers/19_program.c
@@ -9,25 +9,34 @@ int main()
     for(int i = 0; i<5; i++) a1[i] = i;
     for(int i = 0; i<5; i++) a2[i] = i;
 
-    printf("a1: %p\n", a1);
-    printf("a2: %p\n", a2);
+    // printf("a1: %p\n", a1);
+    // printf("a2: %p\n", a2);
+    // for(int i = 0; i < 21; i++){
+    //     printf("a[%d] = %d\n", i, a1[i]);
+    // }
 
-    for(int i = 0; i < 21; i++){
-        printf("a[%d] = %d\n", i, a1[i]);
-    }
-    
     printf("a1: %p\n", a1);
-    
+
     int *save = a1;
-
     a1 = realloc(a1, sizeof(int) * 20);
     
     printf("a1: %p\n", a1);
+    printf("save: %p\n", save);
 
-    for(int i = 0; i < 25; i++){
-        printf("a[%d] = %d\n", i, a1[i]);
+    // for(int i = 0; i < 25; i++){
+    //     printf("a[%d] = %d\n", i, a1[i]);
+    // }
+
+    for(int i = 5; i < 20; i++) a1[i] = i;
+    for(int i = 0; i < 5; i++){
+        printf("save[%d] = %d\n", i, save[i]);
+
     }
-    
+    for(int i = 0; i < 5; i++){
+        printf("a1[%d] = %d\n", i, a1[i]);
+    }
+
+
     free(a1);
     free(a2);  
     return 0;
