d92965fe4f91a993828fcf8c6ee01e811d8af044
arif-z04 <smarifuzzaman123@gmail.com>
2024-09-13 12:46:07 +0600

    what am I doing with my life?

--- Pointers/Dynamic Memory allocation/04_program.c
+++ Pointers/Dynamic Memory allocation/04_program.c
@@ -16,6 +16,23 @@ int main(){
 
     printf("p1 addr: %p\np2 addr: %p\n",
                 password1, password2);
+    // printf("p1 addr: %d\np2 addr: %d\n",
+    //             (int) password1,
+    //             (int) password2);
 
+    for(int i = 0; i < 100; i++){
+        printf("%c", password1[i]);
+    }
+    printf("\n");
+
+    password1 = realloc(password1, 20 * sizeof(char));
+    printf("password1: %s\n\n", password1);
+    printf("p1 addr: %p\np2 addr: %p\n",
+                password1, password2);
+
+    for(int i = 0; i < 100; i++){
+        printf("%c", password1[i]);
+    }
+    
     return 0;
 }   
