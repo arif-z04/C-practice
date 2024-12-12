b8b8359f7e2c89840698f16d235fa65c5db86f53
S.M.Arifuzzaman <smarifuzzaman123@gmail.com>
2024-12-12 17:32:21 +0600

    some-changes

--- CIT LAB/BASIC Declarations and Expressions/2302054_08.c
+++ CIT LAB/BASIC Declarations and Expressions/2302054_08.c
@@ -6,21 +6,12 @@ int main()
     printf("Number of days: ");
     scanf("%d", &days);
 
-    int years = 0; 
-    int weeks = 0;
+    int years = days / 365;
+    days %= 365; 
+    int weeks = days / 7;
+    days %= 7;
 
-    while(days>=30){
-        if(days >= 365){
-            years = days / 365;
-            days %= 365;
-            continue;
-        }
-        if(days >= 7){
-            weeks = days / 7;
-            days %= 7;
-            continue;
-        } 
-    }
+
     printf("Years: %d\nWeeks: %d\nDays: %d", years, weeks, days);
     return 0;
 }
