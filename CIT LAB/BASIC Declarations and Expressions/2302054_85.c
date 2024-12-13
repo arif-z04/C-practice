60b04ad27c1c2500faffcdd0c03683fcb919403b
S.M.Arifuzzaman <smarifuzzaman123@gmail.com>
2024-12-13 15:50:02 +0600

    -.-

--- CIT LAB/BASIC Declarations and Expressions/2302054_85.c
+++ CIT LAB/BASIC Declarations and Expressions/2302054_85.c
@@ -14,27 +14,24 @@ char* convertToRoman(int num){
     }
 
     int pos = 0;
-
     for(int i = 0; i < 9; i++){
-     
         while(num >= values[i]){
-     
             for(int j = 0; ch[i][j] != '\0'; j++){ // processes all the characters
                 result[pos] = ch[i][j];
                 pos++;
             }
-
             num = num - values[i];
-     
         }
     }
     result[pos] = '\0';
     return result;
 }
+
 int main()
 {
     for(int i = 0; i <= 100; i++){
         printf("%d\t%s\n", i, convertToRoman(i));
     }
+
     return 0;
 }
